8cd42421ea987253af72b6d0b163eca3f4527ba9
Lucyloules <lucia.lagos@gmail.com>
2018-01-29 18:52:34 -0300

    version corregida y funcionando

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,34 +1,11 @@
-
-//-------Maite---no puedo hacer que afecte el filtro a la imagen
-const filterOption = document.getElementById("filter-option");
-const filteredImage= document.getElementById("filtered-image");
-const demoImg = document.getElementById("demo-img");
-
-filterOption.appendChild("filteredImage");
-filteredImage.appendChild("demoImg");
-
-option.addEventListener('click', function(){
-
-});
-//----------------filtro invert----------------
-    demoImg.invert = function(pixels) {
-  for (var i = 0; i < pixels.data.length; i += 4) {
-    pixels.data[i] = 255 - pixels.data[i]
-    pixels.data[i+1] = 255 - pixels.data[i+1]
-    pixels.data[i+2] = 255 - pixels.data[i+2]
-  }
-
-  return pixels
-}
-
 // 1.-llamo elementos 
 const container = document.getElementById('primaryContainer');
 const img = document.getElementById('demo');
 
-//2.- creo contenedor secundario y agrego id
+/* //2.- creo contenedor secundario y agrego id
   const secondaryContainer =document.createElement('div');
-  secondaryContainer.setAttribute('id', 'secondaryContainer');*/
-//llamo al contenedor secundario
+  secondaryContainer.setAttribute('id', 'secundary');*/
+// 2.- llamo al contenedor secundario
 secondaryContainer = document.getElementById('secondaryContainer');
 
 // 3.1 creo boton blanco y negro (elemrntos, texto y atributos)
@@ -46,16 +23,15 @@ const btnInvertColors = document.createElement('button');
 const txtInvertColors = document.createTextNode('Invert Colors');
 btnInvertColors.setAttribute('class', 'btn btn-primary');
 
-// 3.4 creo boton Blur (elemrntos, texto y atributos)
-
+// 3.4 creo boton filtro rojo (elemrntos, texto y atributos)
 const btnBlur = document.createElement('button');
 const txtBlur = document.createTextNode('Blur');
 btnBlur.setAttribute('class', 'btn btn-primary');
+
  
 // no pude crear un contenedor secundario con insert y no se veian los botones.
 //const reference = document.getElementById('primaryContainer');
  //document.body.insertBefore(secondaryContainer, reference); 
-
 
 // appendeo los botones
 secondaryContainer.appendChild(btnBlackWhite);
@@ -93,17 +69,16 @@ btnBlur.addEventListener('click', ()=>{
 //funcion que restaurar imagen (no se si crear un nuevo botón
 // o darle la funcionalidad  a la misma imagen)
 /*btnRestore.addEventListener('click', ()=>{
-	img.style.filter='grayscale(0%)'; 
-	img.style.filter='invert(0%)';
-	img.style.filter='sepia(0%)';
-	img.style.filter='blur(5px)';
-
+  img.style.filter='grayscale(0%)'; 
+  img.style.filter='invert(0%)';
+  img.style.filter='sepia(0%)';
+  img.style.filter='blur(5px)';
 });*/
 
 img.addEventListener('click', ()=>{
-	img.style.filter='grayscale(0%)'; 
-	img.style.filter='invert(0%)';
-	img.style.filter='sepia(0%)';
-	img.style.filter='blur(-5px)';
+  img.style.filter='grayscale(0%)'; 
+  img.style.filter='invert(0%)';
+  img.style.filter='sepia(0%)';
+  img.style.filter='blur(-5px)';
 
 });
