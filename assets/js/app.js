c598bacff063b57f4c6b2b0e0caf40672ea50858
Meliza-fb <maite.e.figue@hotmail.com>
2018-01-31 22:18:01 -0300

    Test listo

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,7 @@
+document.body.onload = IBookstore;
 
 function IBookstore(){
+
 // 1.-llamo elementos
 const container = document.getElementById('primaryContainer');
 const img = document.getElementById('demo');
@@ -15,23 +17,18 @@ const btnBlackWhite = document.createElement('button');
 const txtBlackWhite = document.createTextNode('Black and White');
 btnBlackWhite.setAttribute('class', 'btn btn-primary');
 btnBlackWhite.setAttribute('id', 'black');
-
-/*if ('black'== null) {
-  black.filter(true)
-}else{
-  return("error");
+/*if('black' !== null){
+  black.filter(true){
+  }else {
+    return('Error')
+  }
 }*/
+
 // 3.2 creo boton sepia (elemrntos, texto y atributos)
 const btnSepia = document.createElement('button');
 const txtSepia = document.createTextNode('Sepia');
 btnSepia.setAttribute('class', 'btn btn-primary');
 btnSepia.setAttribute('id', 'sepia');
-
-/*if ('sepia'== null) {
-  sepia.filter(true)
-}else{
-  return("error");
-}*/
 
 // 3.3 creo boton invertir colores (elemrntos, texto y atributos)
 const btnInvertColors = document.createElement('button');
@@ -39,22 +36,12 @@ const txtInvertColors = document.createTextNode('Invert Colors');
 btnInvertColors.setAttribute('class', 'btn btn-primary');
 btnInvertColors.setAttribute('id', 'invert');
 
-/*if ('invert'== null) {
-  invert.filter(true)
-}else{
-  return("error");
-}*/
-
 // 3.4 creo boton filtro rojo (elemrntos, texto y atributos)
 const btnBlur = document.createElement('button');
 const txtBlur = document.createTextNode('Blur');
 btnBlur.setAttribute('class', 'btn btn-primary');
 btnBlur.setAttribute('id', 'blur');
-/*if ('blur'== null) {
-  blur.filter(true)
-}else{
-  return("error");
-}*/
+
 
 // no pude crear un contenedor secundario con insert y no se veian los botones.
 //const reference = document.getElementById('primaryContainer');
@@ -75,3 +62,44 @@ btnBlur.appendChild(txtBlur);
 
 
 //Modificacion de imagen, funciones
+
+
+btnBlackWhite.addEventListener('click', ()=>{
+    img.style.filter='grayscale(100%)'; //cambia a blanco y negro
+  });
+
+btnSepia.addEventListener('click', ()=>{
+    img.style.filter='sepia(100%)'; // cambia a sepia
+  });
+
+btnInvertColors.addEventListener('click', ()=>{
+    img.style.filter='invert(100%)'; // invierte colores de una imagen
+  });
+
+btnBlur.addEventListener('click', ()=>{
+    //img.style.filter='saturate(200%)'; saturar de color
+    img.style.filter='blur(5px)'; // desenfoque
+  });
+
+//funcion que restaurar imagen (no se si crear un nuevo botón
+// o darle la funcionalidad  a la misma imagen)
+/*btnRestore.addEventListener('click', ()=>{
+  img.style.filter='grayscale(0%)';
+  img.style.filter='invert(0%)';
+  img.style.filter='sepia(0%)';
+  img.style.filter='blur(5px)';
+});*/
+
+img.addEventListener('click', ()=>{
+  img.style.filter='grayscale(0%)';
+  img.style.filter='invert(0%)';
+  img.style.filter='sepia(0%)';
+  img.style.filter='blur(-5px)';
+});
+
+//se agrega una función para darle estilo a los elementos
+ document.body.onload = addStyle;
+  function addStyle(element) {
+   /* container.style.marginTop = '2%';*/
+  }
+}
--- test/test.spec.js
+++ test/test.spec.js
@@ -1,11 +1,25 @@
-/*const assert= require("chai").assert;
-const IBookstore =require('../assets/app.js');
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
+let dom = new JSDOM('<!DOCTYPE html><html><body><section><div class="row"><div class="col-lg-12 col-md-12 col-xs-12"><div id="secondaryContainer" align="center"></div></div></div><div class="row"><div class="col-lg-12 col-md-12 col-xs-12"><div id="primaryContainer" align="center"><img id="demo" src="assets/img/demo.jpg" alt="photo demo"></div></div></div></section></body></html>');
+const { window } = dom;
+global.document = dom;
+global.window = window;
+global.navigator = {
+ userAgent: 'node.js',
+};
 
-describe(IBookstore, ()=>{
-  it(IBookstore==null){
-    assert.equal(true, IBookstore.black('black')
-  }
-  filtro();
-});
+const $=require('jquery');
 
+describe('testeando', ()=>{
+  it('pasa cuando el body tiene hijos', function(){
+    dom.window.document.body.children.length>=1;
+  });
+
+  it('pasa si la imagen tiene alt', function(){
+    $('#demo').attr('alt') !=='';
+  });
+
+  it('pasa si la imagen tiene src', function(){
+    $('#demo').attr('src') !=='';
+  });
 });
